0d0e21879b0eacbe99e16b73bc8a9fa57e1949be
JenniferVirginiya <49671906+JenniferVirginiya@users.noreply.github.com>
2019-04-16 16:07:51 +0530

    maven files

---
+++ src/main/java/jenkins1/FirstJavaProgram.java
@@ -0,0 +1,14 @@
+package jenkins1;
+
+public class FirstJavaProgram {
+
+	public FirstJavaProgram() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("HelloJava");
+	}
+
+}
